07446297a982478b7c67f0a7c6e75c0f4ea5822a
onur akyıldız <onurakyildi41@gmail.com>
2022-12-12 09:14:57 +0300

    RFID Attendance Systen

--- RFID Öğrenci Yoklama Sistemi/SaveDataToCard/SaveDataToCard.ino
+++ RFID Öğrenci Yoklama Sistemi/SaveDataToCard/SaveDataToCard.ino
@@ -1,212 +1,177 @@
-
 #include <SPI.h>
 #include <MFRC522.h>
-//--------------------------------------------------
+
 //GPIO 0 --> D3
 //GPIO 2 --> D4
 const uint8_t RST_PIN = D3;
 const uint8_t SS_PIN = D4;
-//--------------------------------------------------
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;        
-//--------------------------------------------------
-/* Be aware of Sector Trailer Blocks */
+
+/* blok numara başlangıcı */
 int blockNum = 4;
 byte block_data[16];
-/* Create array to read data from Block */
-/* Length of buffer should be 4 Bytes 
-more than the size of Block (16 Bytes) */
+/* karttaki verileri dizi şeklinde okuyup yazma için */
 byte bufferLen = 18;
 byte readBlockData[18];
-//--------------------------------------------------
-MFRC522::StatusCode status;
-//--------------------------------------------------
 
+MFRC522::StatusCode status;
 
 
 void setup() 
 {
-  //------------------------------------------------------
-  //Initialize serial communications with PC
+  /* seri haberleşme */
   Serial.begin(9600);
-  //------------------------------------------------------
-  //Initialize SPI bus
   SPI.begin();
-  //------------------------------------------------------
-  //Initialize MFRC522 Module
+  
   mfrc522.PCD_Init();
-  Serial.println("Scan a MIFARE 1K Tag to write data...");
-  //------------------------------------------------------
+  Serial.println("kartı okutup işlem bitene kadar kaldırmayınız ...");
+  
 }
 
 
-
-/****************************************************************************************************
- * loop() function
- ****************************************************************************************************/
 void loop()
 {
-  //------------------------------------------------------------------------------
-  /* Prepare the ksy for authentication */
-  /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
+  
+  /* doğrulama */
   for (byte i = 0; i < 6; i++){
     key.keyByte[i] = 0xFF;
   }
-  //------------------------------------------------------------------------------
-  /* Look for new cards */
-  /* Reset the loop if no new card is present on RC522 Reader */
+  
+  /* yeni kart tarama */
+  /* yeni kart okutulunca reset işlemi */
   if ( ! mfrc522.PICC_IsNewCardPresent()){return;}
-  //------------------------------------------------------------------------------
-  /* Select one of the cards */
+  
+  /* okunan kartı seçme */
   if ( ! mfrc522.PICC_ReadCardSerial()) {return;}
-  //------------------------------------------------------------------------------
   Serial.print("\n");
-  Serial.println("**Card Detected**");
-  /* Print UID of the Card */
-  Serial.print(F("Card UID:"));
+  Serial.println("**kart algılandı**");
+  
+  /* kart UID  */
+  Serial.print(F("Kart UID:"));
   for (byte i = 0; i < mfrc522.uid.size; i++){
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
   }
   Serial.print("\n");
-  /* Print type of card (for example, MIFARE 1K) */
-  Serial.print(F("PICC type: "));
+
+  /* kart tipi */
+  Serial.print(F("PICC tip: "));
   MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
   Serial.println(mfrc522.PICC_GetTypeName(piccType));
-    
-  //------------------------------------------------------------------------------
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+  /* bloklara yazılan veriler */
   blockNum = 4;
   toBlockDataArray("191307026"); //Student ID
   WriteDataToBlock(blockNum, block_data);
   ReadDataFromBlock(blockNum, readBlockData);
   dumpSerial(blockNum, readBlockData);
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
   blockNum = 5;
   toBlockDataArray("Onur Akyıldız"); //First Name
   WriteDataToBlock(blockNum, block_data);
   ReadDataFromBlock(blockNum, readBlockData);
   dumpSerial(blockNum, readBlockData);
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
   blockNum = 6;
   toBlockDataArray("Kocaeli Uni"); //Last Name
   WriteDataToBlock(blockNum, block_data);
   ReadDataFromBlock(blockNum, readBlockData);
   dumpSerial(blockNum, readBlockData);
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
   blockNum = 8;
-  toBlockDataArray("Teknoloji fak"); //Father's Name
+  toBlockDataArray("Teknoloji fak"); //Faculty Name
   WriteDataToBlock(blockNum, block_data);
   ReadDataFromBlock(blockNum, readBlockData);
   dumpSerial(blockNum, readBlockData);
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
   blockNum = 9;
-  toBlockDataArray("Bil Sis Müh"); //Date of Birth
+  toBlockDataArray("Bil Sis Müh"); //Department Name
   WriteDataToBlock(blockNum, block_data);
   ReadDataFromBlock(blockNum, readBlockData);
   dumpSerial(blockNum, readBlockData);
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
   
-  //MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 }
 
-
-
-/****************************************************************************************************
- * Writ() function
- ****************************************************************************************************/
 void WriteDataToBlock(int blockNum, byte blockData[]) 
 {
-   Serial.print("Writing data on block ");
-   Serial.print(blockNum);
-  //------------------------------------------------------------------------------
-  /* Authenticating the desired data block for write access using Key A */
+  Serial.println("bloklara veri yazılıyor ... ");
+  Serial.println("blok numrası : "+blockNum);
+
+  /* yazma erişimi için istenen veri bloğunun kimliğinin doğrulanması */
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK){
-    Serial.print("Authentication failed for Write: ");
+  if (status != MFRC522::STATUS_OK)
+  {
+    Serial.println("başarısız: ");
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
-  //------------------------------------------------------------------------------
-  else {
-    Serial.println("Authentication success");
+  else 
+  {
+    Serial.println("başarılı");
   }
-  //------------------------------------------------------------------------------
-  /* Write data to the block */
+  
+  /* bloka verileri yazma */
   status = mfrc522.MIFARE_Write(blockNum, blockData, 16);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print("Writing to Block failed: ");
+  if (status != MFRC522::STATUS_OK)
+  {
+    Serial.println("başarısız: ");
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
   else
-  {Serial.println("Data was written into Block successfully");}
-  //------------------------------------------------------------------------------
+  {
+    Serial.println("yazma başarılı");
+  }
 }
 
 
-
-
-
-/****************************************************************************************************
- * ReadDataFromBlock() function
- ****************************************************************************************************/
 void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
 {
-   Serial.print("Reading data from block ");
-   Serial.println(blockNum);
-  //------------------------------------------------------------------------------
-  /* Authenticating the desired data block for Read access using Key A */
+   Serial.println("bloktan veri okunuyor... ");
+   Serial.println("blok numarası: "+blockNum);
+
+  /* Okuma erişimi için istenen veri bloğunun kimliğinin doğrulanması */
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
-  //------------------------------------------------------------------------------
-  if (status != MFRC522::STATUS_OK){
-   Serial.print("Authentication failed for Read: ");
+  if (status != MFRC522::STATUS_OK)
+  {
+   Serial.println("başarısız: ");
    Serial.println(mfrc522.GetStatusCodeName(status));
    return;
   }
-  else {
-    Serial.println("Authentication success");
+  else
+  {
+    Serial.println("başarılı");
   }
-  //------------------------------------------------------------------------------
-  /* Reading data from the Block */
+
+  /* bloktan veri okuma */
   status = mfrc522.MIFARE_Read(blockNum, readBlockData, &bufferLen);
-  if (status != MFRC522::STATUS_OK){
-    Serial.print("Reading failed: ");
+  if (status != MFRC522::STATUS_OK)
+  {
+    Serial.println("başarısız: ");
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
   else {
-    Serial.println("Block was read successfully");  
+    Serial.println("okuma başarılı");  
   }
-  //------------------------------------------------------------------------------
 }
 
-
-
-/****************************************************************************************************
- * dumpSerial() function
- ****************************************************************************************************/
 void dumpSerial(int blockNum, byte blockData[]) 
 {
    Serial.print("\n");
-   Serial.print("Data in Block:");
+   Serial.print("bloktaki veri:");
    Serial.print(blockNum);
    Serial.print(" --> ");
-   for (int j=0 ; j<16 ; j++){
+   for (int j=0 ; j<16 ; j++)
+   {
      Serial.write(readBlockData[j]);
    }
-   Serial.print("\n");Serial.print("\n");
+   Serial.print("\n");
+   Serial.print("\n");
 }
 
-
-/****************************************************************************************************
- * dumpSerial() function
- ****************************************************************************************************/
 void toBlockDataArray(String str) 
 {
   byte len = str.length();
--- RFID Öğrenci Yoklama Sistemi/SaveDataToSheet/SaveDataToSheet.ino
+++ RFID Öğrenci Yoklama Sistemi/SaveDataToSheet/SaveDataToSheet.ino
@@ -4,208 +4,217 @@
 #include <MFRC522.h>
 #include <HTTPSRedirect.h>
 
-#include<Wire.h>
-#include<LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const char *GScriptId = "AKfycbxHt2ieEhUjfU7IvundtmcPZws3C0Me1QAQJJrc-5v78onl9bc2F5X-alX7B3gczNM1qQ";
+// Google Script deploy kimliği:
+const char *GScriptId = "AKfycbyqQOre8jZbxRNB6tQLhefHS6rL98v2Cfq2Uk0SEZXH5ffxy1Ied22ubzr6sbp0DUnHRg";
 
+// wifi bilgileri:
 const char* ssid     = "FiberHGW_ZT2Y7A_2.4GHz";
 const char* password = "pjCeU4xeTa";
 
 String payload_base =  "{\"command\": \"insert_row\", \"sheet_name\": \"Sayfa1\", \"values\": ";
 String payload = "";
 
+// Google Sheets setup (do not edit)
 const char* host        = "script.google.com";
 const int   httpsPort   = 443;
 const char* fingerprint = "";
 String url = String("/macros/s/") + GScriptId + "/exec";
 HTTPSRedirect* client = nullptr;
 
+// sheete gidicek verilerin tanımlanması
 String student_id;
 
 int blocks[] = {4,5,6,8,9};
 #define total_blocks  (sizeof(blocks) / sizeof(blocks[0]))
 
-#define RST_PIN  D3 //D3
+#define RST_PIN  D3  //D3
 #define SS_PIN   D4  //D4
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;  
 MFRC522::StatusCode status;
 
 int blockNum = 2;  
-
+/* verileri dizi şeklince okucağız*/
 byte bufferLen = 18;
 byte readBlockData[18];
 
-void setup() {
+void setup()
+{
+  /*seri haberleşme*/
   Serial.begin(9600);        
   delay(10);
   Serial.println('\n');
+  
   SPI.begin();
   
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  lcd.setCursor(0,0); 
-  lcd.print("Connecting to");
-  lcd.setCursor(0,1); 
-  lcd.print("WiFi...");
-  
+  // Connect to WiFi
   WiFi.begin(ssid, password);             
   Serial.print("Connecting to ");
   Serial.print(ssid); Serial.println(" ...");
   
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.print(".");
   }
-  
   Serial.println('\n');
   Serial.println("Connection established!");  
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
   
+  // Use HTTPSRedirect class to create a new TLS connection
   client = new HTTPSRedirect(httpsPort);
   client->setInsecure();
   client->setPrintResponseBody(true);
   client->setContentTypeHeader("application/json");
-  
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Connecting to");
-  lcd.setCursor(0,1); 
-  lcd.print("Google ");
   delay(5000);
   
   Serial.print("Connecting to ");
   Serial.println(host);
-  bool flag = false;
   
-  for(int i=0; i<5; i++){ 
+  bool flag = false;
+  for(int i=0; i<5; i++)
+  { 
     int retval = client->connect(host, httpsPort);
-    if (retval == 1){
+    if (retval == 1)
+    {
       flag = true;
       String msg = "Connected. OK";
       Serial.println(msg);
-      lcd.clear();
-      lcd.setCursor(0,0); 
-      lcd.print(msg);
-      delay(2000);
+      
       break;
     }
     else
+    {
       Serial.println("Connection failed. Retrying...");
+    }
   }
-  if (!flag){
-    lcd.clear();
-    lcd.setCursor(0,0); //col=0 row=0
-    lcd.print("Connection fail");
+  if (!flag)
+  {
+    
     Serial.print("Could not connect to server: ");
     Serial.println(host);
     delay(5000);
     return;
   }
-  
-  delete client;  
-  client = nullptr; 
+  delete client;    
+  client = nullptr;
 }
-void loop() {
+
+void loop() 
+{
   static bool flag = false;
-  if (!flag){
+  if (!flag)
+  {
     client = new HTTPSRedirect(httpsPort);
     client->setInsecure();
     flag = true;
     client->setPrintResponseBody(true);
     client->setContentTypeHeader("application/json");
   }
-  if (client != nullptr){
+  if (client != nullptr)
+  {
     if (!client->connected())
-      {client->connect(host, httpsPort);}
+      {
+        client->connect(host, httpsPort);
+      }
   }
-  else{Serial.println("Error creating client object!");}
-  lcd.clear();
-  lcd.setCursor(0,0); 
-  lcd.print("Scan your Tag");
+  else
+  {
+    Serial.println("Error creating client object!");
+  }
+ 
   
   mfrc522.PCD_Init();
+  /* kart tarama */
+  /* kart algılandığında reset işlemi */
   if ( ! mfrc522.PICC_IsNewCardPresent()) {return;}
+  /* kartı seçme */
   if ( ! mfrc522.PICC_ReadCardSerial()) {return;}
+  /* bloktan veriyi okuma */
   Serial.println();
-  Serial.println(F("Reading last data from RFID..."));  
-  
+  Serial.println(F("RFID okunuyor..."));  
+ 
   String values = "", data;
-  for (byte i = 0; i < total_blocks; i++) {
+
+ 
+  //url oluşturma
+  for (byte i = 0; i < total_blocks; i++)
+  {
     ReadDataFromBlock(blocks[i], readBlockData);
-    
-    if(i == 0){
+    if(i == 0)
+    {
       data = String((char*)readBlockData);
       data.trim();
       student_id = data;
       values = "\"" + data + ",";
     }
-    else if(i == total_blocks-1){
+    else if(i == total_blocks-1)
+    {
       data = String((char*)readBlockData);
       data.trim();
       values += data + "\"}";
     }
-    else{
+    else
+    {
       data = String((char*)readBlockData);
       data.trim();
       values += data + ",";
     }
   }
   
+  // json objesi oluşturup sheete yazma
+  // values = "\"" + value0 + "," + value1 + "," + value2 + "\"}"
   payload = payload_base + values;
-  lcd.clear();
-  lcd.setCursor(0,0); //col=0 row=0
-  lcd.print("Publishing Data");
-  lcd.setCursor(0,1); //col=0 row=0
-  lcd.print("Please Wait...");
-  Serial.println("Publishing data...");
-  Serial.println(payload);
   
-  if(client->POST(url, host, payload)){ 
-    lcd.clear();
-    lcd.setCursor(0,0); //col=0 row=0
-    lcd.print("Student ID: "+student_id);
-    lcd.setCursor(0,1); //col=0 row=0
-    lcd.print("Present");
+ 
+  
+  // sheete göndeme
+  Serial.println("veri gönderiliyor...");
+  Serial.println(payload);
+  if(client->POST(url, host, payload))
+  { 
+      //lcd kodları olucak
   }
-  else{
-    Serial.println("Error while connecting");
-    lcd.clear();
-    lcd.setCursor(0,0); //col=0 row=0
-    lcd.print("Failed.");
-    lcd.setCursor(0,1); //col=0 row=0
-    lcd.print("Try Again");
-  } 
+  else
+  {
+    Serial.println("hata");
+  }   
   delay(5000);
 }
 
 void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
 { 
-  for (byte i = 0; i < 6; i++) {
+  for (byte i = 0; i < 6; i++) 
+  {
     key.keyByte[i] = 0xFF;
   }
+  /* Okuma erişimi için istenen veri bloğunun kimliğinin doğrulanması */
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK){
-     Serial.print("Authentication failed for Read: ");
+  
+  if (status != MFRC522::STATUS_OK)
+  {
+     Serial.print("okuma hatalı: ");
      Serial.println(mfrc522.GetStatusCodeName(status));
      return;
   }
-  else {
-    Serial.println("Authentication success");
+  else
+  {
+    Serial.println("okuma başarılı");
   }
+  /* bloktan veri okuma */
   status = mfrc522.MIFARE_Read(blockNum, readBlockData, &bufferLen);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print("Reading failed: ");
+  if (status != MFRC522::STATUS_OK) 
+  {
+    Serial.print("okuma hatalı: ");
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
-  else {
+  else 
+  {
     readBlockData[16] = ' ';
     readBlockData[17] = ' ';
-    Serial.println("Block was read successfully");  
+    Serial.println("başarılı");  
   }
 }
